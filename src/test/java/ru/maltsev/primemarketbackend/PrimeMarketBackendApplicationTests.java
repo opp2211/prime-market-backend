e5fc446c3efad179daf88a4eaa4130848f84d5c4
Kirill Maltsev <opp.2211@gmail.com>
2026-02-14 21:42:01 +0500

    Initial commit

---
+++ src/test/java/ru/maltsev/primemarketbackend/PrimeMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maltsev.primemarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeMarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
